a3ecb51957047e5a9ea25f8bf3c9f734243fbbf7
linhptt <linhptt@LINHPTT-KHDD>
2024-09-28 08:38:43 +0700

    Add project files.

---
+++ Baitap2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baitap2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//bai 1
+         //    int a = 12, b = 6,c = a + b,d = a - b, e = a * b, f = a / b;
+         //    Console.WriteLine("tong cua hai so a va b la {0}", c);
+         //    Console.WriteLine("hieu cua hai so a va b la {0}", d);
+         //    Console.WriteLine("tich cua hai so a va b la {0}", e);
+         //    Console.WriteLine("thuong cua hai so a va b la {0}", f);
+         //    Console.ReadKey();
+         //}
+         //{//Bai 2
+         //    int a = 4, b = 3, c = a * b, d = 2 * (a + b);
+         //    Console.WriteLine("Chieu dai cua hinh chu nhat la {0} cm", a);
+         //    Console.WriteLine("Chieu rong cua hinh chu nhat la {0} cm", b);
+         //    Console.WriteLine("Dien tich cua hinh chu nhat la {0} cm vuong", c);
+         //    Console.WriteLine("Chu vi cua hinh chu nhat la {0} cm", d);
+         //    Console.ReadKey();
+         //}
+         //{
+         //    //Bai 3
+         //    double a = 8, b = a / 2, c = 2 * Math.PI * b, d = Math.PI * Math.Pow(b, 2);
+         //    Console.WriteLine("Duong kinh duong tron la {0} met", a);
+         //    Console.WriteLine("Ban kinh duong tron la {0} met", b);
+         //    Console.WriteLine("Chu vi duong tron la {0:F2} met", c);
+         //    Console.WriteLine("Dien tich duong tron la {0:F2} met vuong", d);
+         //    Console.ReadKey();
+
+            //}
+            //{
+            //    //Bai 4
+            //    string Hoten = "Pham Thi Thuy Linh",Lop = "9A";
+            //    float HK1 = 8, HK2 = 9,Ca_nam = (HK1 + HK2 * 2) / 3;
+            //    Console.WriteLine("Ho va ten:{0}", Hoten);
+            //    Console.WriteLine("Lop:{0}", Lop);
+            //    Console.WriteLine("Diem trung binh hoc ky 1 la {0:F2}", HK1);
+            //    Console.WriteLine("Diem trung binh hoc ky 2 la {0:F2}", HK2);
+            //    Console.WriteLine("Diem trung binh ca nam la {0:F2}", Ca_nam);
+            //    Console.ReadKey();
+
+            //}
+            //{ //Bai 5
+            //    string hoten, gt;
+            //    DateTime ns;
+            //    float hesoluong;
+            //    int luongcoban, sonam, phucap;
+
+
+            //    hoten = "Nguyen Van A";
+            //    gt = "Nam";
+            //   ns = new DateTime(1987, 12, 10);
+            //    hesoluong = 2.0f;
+            //    luongcoban = 5000000;
+            //    sonam = 6;
+            //    if (sonam > 5)
+            //        phucap = luongcoban / 2;
+            //    else
+            //        phucap = 0;
+            //    Console.WriteLine("ho va ten:{0}\ngioi tinh:{1}", hoten,gt);
+            //    Console.WriteLine("ngay sinh:{0}", ns.ToString("dd/MM/yyyy"));
+            //    Console.WriteLine("he so luong:{0}", hesoluong);
+            //    Console.WriteLine("luong co ban:{0}", luongcoban);
+            //    Console.WriteLine("so nam lam viec: {0} nam", sonam);
+            //    Console.WriteLine("phu cap: {0}", phucap);
+
+            //    hoten = "Nguyen Thi B";
+            //    gt = "Nu";
+            //    ns = new DateTime(1985, 12, 10);
+            //    hesoluong = 2.5f;
+            //    luongcoban = 5000000;
+            //    sonam = 4;
+            //    if (sonam > 5)
+            //        phucap = luongcoban / 2;
+            //    else
+            //        phucap = 0;
+            //    Console.WriteLine("ho va ten:{0}\ngioi tinh:{1}", hoten, gt);
+            //    Console.WriteLine("ngay sinh:{0}", ns.ToString("dd/MM/yyyy"));
+            //    Console.WriteLine("he so luong:{0}", hesoluong);
+            //    Console.WriteLine("luong co ban:{0}", luongcoban);
+            //    Console.WriteLine("so nam lam viec: {0} nam", sonam);
+            //    Console.WriteLine("phu cap: {0}", phucap);
+
+            //    Console.ReadKey();
+            //}
+            //{ //Bai 6
+
+            //    DateTime inputDate = new DateTime(2023, 03, 19);
+            //    DateTime yesterday = inputDate.AddDays(-1);
+            //    DateTime tomorrow = inputDate.AddDays(1);
+
+            //    Console.WriteLine("Ngày hôm qua: " + yesterday.ToString("dd/MM/yyyy"));
+            //    Console.WriteLine("Ngày hien tai: " + inputDate.ToString("dd/MM/yyyy"));
+            //    Console.WriteLine("Ngày mai: " + tomorrow.ToString("dd/MM/yyyy"));
+            //    Console.ReadKey();
+        }
+    }
+}
